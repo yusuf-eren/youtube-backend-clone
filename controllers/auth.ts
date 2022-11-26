171e4302b1f4399f982242de595981b838ffd73e
yusuf-eren <erenyusuf170@gmail.com>
2022-11-26 18:43:20 +0300

    cookie

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -4,44 +4,60 @@ import { User } from '../models/user';
 import { Password } from '../services/password';
 
 export const signupController = async (req: Request, res: Response) => {
-    const { name, email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).send('Email in use');
+    try {
+        const { name, email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (existingUser) return res.status(400).send('Email in use');
 
-    const user = await User.create({ name, email, password });
+        const user = await User.create({ name, email, password });
 
-    const userJwt = jwt.sign(
-        {
-            id: user._id,
-            email: user.email,
-        },
-        process.env.JWT_KEY!
-    );
+        const userJwt = jwt.sign(
+            {
+                id: user._id,
+                email: user.email,
+            },
+            process.env.JWT_KEY!
+        );
 
-    res.set('Set-Cookie', userJwt);
-
-    res.status(201).send(user);
+        res.cookie('jsonwebtoken', userJwt);
+        res.status(201).send(user);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
 };
 export const signinController = async (req: Request, res: Response) => {
-    const { email, password } = req.body;
-    const existingUser = await User.findOne({ email });
-    if (!existingUser) return res.status(400).send('Invalid Credentials');
+    try {
+        const { email, password } = req.body;
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) return res.status(400).send('Invalid Credentials');
 
-    const passwordMatch = await Password.compare(
-        existingUser.password,
-        password
-    );
+        const passwordMatch = await Password.compare(
+            existingUser.password,
+            password
+        );
 
-    if (!passwordMatch) return res.status(400).send('Invalid Credentials');
+        if (!passwordMatch) return res.status(400).send('Invalid Credentials');
 
-    const userJwt = jwt.sign(
-        {
-            id: existingUser.id,
-            email: existingUser.email,
-        },
-        process.env.JWT_KEY!
-    );
+        const userJwt = jwt.sign(
+            {
+                id: existingUser.id,
+                email: existingUser.email,
+            },
+            process.env.JWT_KEY!
+        );
+        console.log();
+        res.cookie('jsonwebtoken', userJwt);
+        res.status(200).send(existingUser);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
+};
 
-    res.set('Set-Cookie', userJwt);
-    res.status(200).send(existingUser);
+export const signoutControler = (req: Request, res: Response) => {
+    try {
+        res.clearCookie('jsonwebtoken');
+        res.sendStatus(200);
+    } catch (error) {
+        return res.status(400).send(error);
+    }
 };
