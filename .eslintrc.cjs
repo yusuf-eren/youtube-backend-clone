788af768e600c6bf406d7bf9e6da6e26955d79bf
yusuf-eren <erenyusuf170@gmail.com>
2022-11-26 18:43:14 +0300

    eslint rule update

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,4 +3,12 @@ module.exports = {
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
     root: true,
-  };
+    rules: {
+        '@typescript-eslint/no-namespace': 'off',
+    },
+    env: {
+        browser: true,
+        amd: true,
+        node: true,
+    },
+};
