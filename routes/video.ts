489d0094e411c93408b5cba25221ac9b72804914
yusuf-eren <erenyusuf170@gmail.com>
2022-11-26 22:08:44 +0300

    video routing

--- routes/video.ts
+++ routes/video.ts
@@ -1,7 +1,16 @@
 import express from 'express';
-import { likeVideo, dislikeVideo } from '../controllers/video'
+import {
+    likeVideo,
+    dislikeVideo,
+    uploadVideo,
+    editVideo,
+    deleteVideo,
+} from '../controllers/video';
 const app = express.Router();
 
+app.post('/upload', uploadVideo);
+app.patch('/edit', editVideo);
+app.delete('/delete', deleteVideo);
 app.route('/:videoId/like').post(likeVideo).delete(dislikeVideo);
 
 export { app as videoRoute };
