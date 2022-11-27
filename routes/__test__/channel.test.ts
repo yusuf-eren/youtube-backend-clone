d5b07e18d23b03b4d9a088a452f75abd6d0ad97e
yusuf-eren <erenyusuf170@gmail.com>
2022-11-27 15:12:22 +0300

    channel tests for subscribing

--- routes/__test__/channel.test.ts
+++ routes/__test__/channel.test.ts
@@ -1,3 +1,59 @@
 import request from 'supertest';
 import { app } from '../../app';
 
+it('subscribes to channel', async () => {
+    const userCookie = await signup();
+    const channel = await createChannel();
+    await request(app)
+        .post(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(200);
+});
+
+it('unsubscribes to channel', async () => {
+    const userCookie = await signup();
+    const channel = await createChannel();
+
+    // Subscribe
+    await request(app)
+        .post(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(200);
+
+    // Unsubscribe
+    await request(app)
+        .delete(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(200);
+});
+
+it('can not unsubscribe if user did not subscribed to channel', async () => {
+    const userCookie = await signup();
+    const channel = await createChannel();
+
+    // Unsubscribe
+    await request(app)
+        .delete(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(400);
+});
+
+it('can not subscribe to channel if user already subscribed', async () => {
+    const userCookie = await signup();
+    const channel = await createChannel();
+    await request(app)
+        .post(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(200);
+
+    await request(app)
+        .post(`/channel/subscribe/${channel._body._id}`)
+        .set('Cookie', userCookie)
+        .send({})
+        .expect(400);
+});
