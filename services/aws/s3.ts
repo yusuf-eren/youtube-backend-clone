19b6f5fb9f5941274e5d21a75461f1a8d7755c7f
yusuf-eren <erenyusuf170@gmail.com>
2022-11-28 00:10:17 +0300

    s3 service

---
+++ services/aws/s3.ts
@@ -0,0 +1,48 @@
+import AWS from 'aws-sdk';
+import { createReadStream } from 'fs';
+import { Video } from '../../models/video';
+import { generateShortID } from '../url';
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: 'eu-central-1',
+});
+
+export const uploadVideo_AWS_S3 = async (videoId: string, file: Buffer) => {
+    const s3 = new AWS.S3();
+    const fileType = 'video/mp4';
+    const fileNameCoded = generateShortID();
+    const stream = file;
+    const s3Params = {
+        Bucket: process.env.VIDEO_BUCKET!,
+        Key: fileNameCoded,
+        ContentType: fileType,
+        Body: stream,
+        ACL: 'public-read',
+    };
+
+    const returnData: any = {
+        url: null,
+    };
+    await new Promise((resolve, reject) => {
+        s3.upload(s3Params, (err: any, data: any) => {
+            if (err) {
+                console.log(err);
+                reject(err);
+            }
+            resolve(data);
+        });
+    }).then((data) => {
+        returnData.url = `https://${process.env.VIDEO_BUCKET}.s3.amazonaws.com/${fileNameCoded}`;
+    });
+
+    await Video.findOneAndUpdate(
+        { _id: videoId },
+        {
+            S3_url: returnData.url,
+        }
+    );
+
+    return returnData;
+};
